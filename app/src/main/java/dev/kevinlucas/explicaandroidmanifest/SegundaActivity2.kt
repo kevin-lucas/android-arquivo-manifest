b3d0dd42e71a368a5715a4f09cb265a924764374
= <kevinlucas.tec.info@gmail.com>
2021-02-17 14:56:21 -0300

    Commit

---
+++ app/src/main/java/dev/kevinlucas/explicaandroidmanifest/SegundaActivity2.kt
@@ -0,0 +1,11 @@
+package dev.kevinlucas.explicaandroidmanifest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SegundaActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_segunda2)
+    }
+}
